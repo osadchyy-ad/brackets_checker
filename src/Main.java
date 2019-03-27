01917a8521a202acabc7e7369dc6e8232c43f079
oasdchyy.ad <osadchyy.ad@gmail.com>
2019-03-28 00:29:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Only for test
+        System.out.println(check("([{}])"));
+        System.out.println(check("([{)"));
+        System.out.println(check("([{]})"));
+        System.out.println(check("((([])))"));
+
+    }
+
+    private static boolean check(String brackets_input) {
+        char[] brackets_array = brackets_input.toCharArray();
+        Stack<Character> brackets_list = new Stack<>();
+        for (char c : brackets_array) {
+            if (c == '(' || c == '[' || c == '{') {
+                brackets_list.push(c);
+            } else if ((c == ')') && (brackets_list.peek() == '(')) {
+                brackets_list.pop();
+            } else if ((c == ']') && (brackets_list.peek() == '[')) {
+                brackets_list.pop();
+            } else if ((c == '}') && (brackets_list.peek() == '{')) {
+                brackets_list.pop();
+            } else {
+                break;
+            }
+        }
+        return brackets_list.isEmpty();
+    }
+}
