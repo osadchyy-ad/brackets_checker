31f01af5dae2e8dbca32088f51997caeb12338c9
oasdchyy.ad <osadchyy.ad@gmail.com>
2019-03-28 01:51:42 +0200

    Fix

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println(check("([{)"));
         System.out.println(check("([{]})"));
         System.out.println(check("((([])))"));
+        System.out.println(check("(([])"));
         System.out.println(check(" "));
         System.out.println(check(""));
     }
